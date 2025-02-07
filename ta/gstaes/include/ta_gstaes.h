59bd1ccdcb6f28fc555f113d9cdbc16bda9dfd7f
Jens Wiklander <jens.wiklander@linaro.org>
2025-02-07 12:04:05 +0100

    ta: add Gstreamer AES TA
    Add a Gstreamer AES TA for decoding buffers for the bad Gstreamer
    gstaesdec module.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

---
+++ ta/gstaes/include/ta_gstaes.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/* Copyright (c) 2025, Linaro Limited */
+
+#ifndef TA_GSTAES_H
+#define TA_GSTAES_H
+
+#define TA_GSTAES_UUID { 0x53ad47f2, 0xe4a3, 0x11ef, \
+			{ 0xb4, 0xc5, 0x7c, 0x21, 0x4a, 0x3a, 0x96, 0x70 } }
+
+#define TA_GSTAES_DEC_CIPHER_INIT	0
+#define TA_GSTAES_DEC_CIPHER_UPDATE	1
+#define TA_GSTAES_DEC_CIPHER_FINAL	2
+
+#endif /* TA_GSTAES_H */
